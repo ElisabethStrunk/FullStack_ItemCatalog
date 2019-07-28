a2e22e10378f75113efb1fb2b1e0963e7a6cf462
Elisabeth Strunk <e.strunk@salt-and-pepper.eu>
2019-07-29 00:10:32 +0200

    base server application that connects to the database + mini-html for testing purposes

--- application.py
+++ application.py
@@ -5,11 +5,31 @@ Item Catalog
 TODO: write description
 """
 
-from .app import *
+from flask import render_template
 
+from app.database_setup import Categories, Items
+from app import configure_app, open_db_session
 
 __author__ = "Elisabeth M. Strunk"
 __version__ = 1.0
 __maintainer__ = "Elisabeth M. Strunk"
 __email__ = "elisabeth.maria.strunk@gmail.com"
 __status__ = "Development"
+
+
+# Initialise app
+app = configure_app()
+
+
+@app.route('/')
+@app.route('/categories')
+def show_categories():
+    session = open_db_session()
+    print("CALLED!!")
+    categories = session.query(Categories).all()
+    return render_template('base.html', categories=categories)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost', port=5000)
