c984dd826cb719c959092305ad9610c0d028cae0
Elisabeth Strunk <e.strunk@salt-and-pepper.eu>
2019-07-28 16:56:00 +0200

    foundation for application: gitignore update to ignore pycharm idea directory; base directory structure; header in main file "application.py" (TODO: detailed info)

---
+++ application.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Item Catalog
+
+TODO: write description
+"""
+
+from .app import *
+
+
+__author__ = "Elisabeth M. Strunk"
+__version__ = 1.0
+__maintainer__ = "Elisabeth M. Strunk"
+__email__ = "elisabeth.maria.strunk@gmail.com"
+__status__ = "Development"
