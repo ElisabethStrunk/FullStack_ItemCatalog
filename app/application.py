360451daf619dfdf8477e96dba0423bb4815adfd
Elisabeth Strunk <e.strunk@salt-and-pepper.eu>
2019-08-01 20:00:00 +0200

    refactored database access, so there are no direct database calls anymore

--- app/application.py
+++ app/application.py
@@ -22,6 +22,13 @@ __email__ = "elisabeth.maria.strunk@gmail.com"
 __status__ = "Development"
 
 
+'''
+CONNECTION TO THE DATABASE
+  *  If the database file does not exist yet, create the database and populate 
+     it with dummy data.
+  *  Open a connection to the database
+  *  Define functions that handle the interaction with the database. 
+'''
 if not os.path.exists('item_catalog.db'):
     from app.database_setup import create_database
     from app.populate_database import populate_database
@@ -36,20 +43,40 @@ db_session = sessionmaker(bind=engine)
 session = db_session()
 
 
-def get_categories():
+def get_categories_from_db():
     return session.query(Categories).all()
 
 
-def get_latest_items():
+def get_latest_items_from_db():
     return session.query(Items).order_by(text("last_modified DESC")).limit(5)
 
 
-def get_items(category):
+def get_items_from_db(category):
     return session.query(Items).filter_by(category=category)
 
 
-def get_item(item_id):
+def get_item_from_db(item_id):
     return session.query(Items).filter_by(id=item_id).one()
+
+
+def edit_item_in_db(edited_item):
+    session.add(edited_item)
+    session.commit()
+    return edited_item
+
+
+def add_item_to_db(item):
+    session.add(item)
+    session.commit()
+    new_item = session.query(Items).filter_by(
+        last_modified=item.last_modified).one()
+    return new_item
+
+
+def delete_item_from_db(item):
+    session.delete(item)
+    session.commit()
+    return None
 
 
 app = Flask(__name__)
@@ -59,8 +86,8 @@ app = Flask(__name__)
 @app.route('/catalog')
 @app.route('/catalog/')
 def index():
-    categories = get_categories()
-    latest_items = get_latest_items()
+    categories = get_categories_from_db()
+    latest_items = get_latest_items_from_db()
     return render_template('index.html', categories=categories,
                            latest_items=latest_items)
 
@@ -68,9 +95,9 @@ def index():
 @app.route('/catalog/<string:category>')
 @app.route('/catalog/<string:category>/items')
 def category(category):
-    if category in [c.name for c in get_categories()]:
-        categories = get_categories()
-        items = get_items(category)
+    if category in [c.name for c in get_categories_from_db()]:
+        categories = get_categories_from_db()
+        items = get_items_from_db(category)
         number_of_items = len(items.all())
         return render_template('category.html', categories=categories,
                                category=category, items=items,
@@ -82,7 +109,7 @@ def category(category):
 
 @app.route('/catalog/<string:category>/<string:item_id>')
 def item(category, item_id):
-    item = get_item(item_id)
+    item = get_item_from_db(item_id)
     if item:
         if item.category != category:
             return jsonify({'message': "The item you requested was not found "
@@ -97,9 +124,9 @@ def item(category, item_id):
 
 @app.route('/catalog/<string:item_id>/edit', methods=['GET', 'POST'])
 def edit_item(item_id):
-    item = get_item(item_id)
+    item = get_item_from_db(item_id)
     if request.method == 'GET':
-        categories = get_categories()
+        categories = get_categories_from_db()
         return render_template('edit_item.html', categories=categories,
                                item=item)
     elif request.method == 'POST':
@@ -110,8 +137,7 @@ def edit_item(item_id):
         if request.form['category']:
             item.category = request.form['category']
         item.last_modified = datetime.datetime.now()
-        session.add(item)
-        session.commit()
+        edit_item_in_db(item)
         return redirect(url_for('item', item_id=item_id,
                                 category=item.category))
     else:
@@ -120,12 +146,11 @@ def edit_item(item_id):
 
 @app.route('/catalog/<string:item_id>/delete', methods=['GET', 'POST'])
 def delete_item(item_id):
-    item = get_item(item_id)
+    item = get_item_from_db(item_id)
     if request.method == 'GET':
         return render_template('delete_item.html', item=item)
     elif request.method == 'POST':
-        session.delete(item)
-        session.commit()
+        delete_item_from_db(item)
         return redirect(url_for('category', category=item.category))
     else:
         return 405
@@ -134,7 +159,7 @@ def delete_item(item_id):
 @app.route('/catalog/add', methods=['GET', 'POST'])
 def add_item():
     if request.method == 'GET':
-        categories = get_categories()
+        categories = get_categories_from_db()
         return render_template('add_item.html', categories=categories)
     elif request.method == 'POST':
         if request.form['name']:
@@ -143,14 +168,13 @@ def add_item():
             item_description = request.form['description']
         if request.form['category']:
             item_category = request.form['category']
-        time = datetime.datetime.now()
-        session.add(Items(name=item_name, description=item_description,
-                          category=item_category,
-                          last_modified=time))
-        session.commit()
-        item = session.query(Items).filter_by(last_modified=time).one()
-        return redirect(url_for('item', item_id=item.id,
-                                category=item.category))
+        new_item = add_item_to_db(
+            Items(name=item_name,
+                  description=item_description,
+                  category=item_category,
+                  last_modified=datetime.datetime.now()))
+        return redirect(url_for('item', item_id=new_item.id,
+                                category=new_item.category))
     else:
         return 405
 
@@ -162,14 +186,14 @@ JSON endpoints:
 
 @app.route('/catalog.json')
 def index_json():
-    categories = get_categories()
+    categories = get_categories_from_db()
     serialized_catalog = {}
     for category in categories:
         serialized_catalog.update(
             {category.name: {}}
         )
         serialized_items = []
-        for item in get_items(category.name):
+        for item in get_items_from_db(category.name):
             serialized_items.append(item.serialize)
         serialized_catalog[category.name].update(
             {'Items': serialized_items}
@@ -180,11 +204,11 @@ def index_json():
 @app.route('/catalog/<string:category>.json')
 @app.route('/catalog/<string:category>/items.json')
 def category_json(category):
-    if category in [c.name for c in get_categories()]:
-        items = get_items(category)
+    if category in [c.name for c in get_categories_from_db()]:
+        items = get_items_from_db(category)
         number_of_items = len(items.all())
         serialized_items = []
-        for item in get_items(category):
+        for item in get_items_from_db(category):
             serialized_items.append(item.serialize)
         serialized_category = {
             'Name': category,
@@ -198,7 +222,7 @@ def category_json(category):
 
 @app.route('/catalog/<string:category>/<string:item_id>.json')
 def item_in_category_json(category, item_id):
-    item = get_item(item_id)
+    item = get_item_from_db(item_id)
     if item:
         if item.category != category:
             return jsonify({'message': "The item you requested was not found "
